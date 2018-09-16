2db18beb07d7b0355d2f7021b3de627d6e1c01ed
Nonato Home <nonato.developer@gmail.com>
2018-09-16 17:08:28 -0300

    testes

--- src/components/views/spending/register/spending-register.vue
+++ src/components/views/spending/register/spending-register.vue
@@ -2,129 +2,79 @@
   <div>
     <div class="layout-padding media-control">
       <div class="row wrap gutter justify-center" style="display: inline-block;">
-  
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/food.jpg">
-        <div class="card-content">
-            Card Content
+        <div class="card-item" v-for="(spendingType, index) in spendingTypeData" :key="index">
+            <component :is="componentCardType" 
+                :img="spendingType.img"
+                :title="spendingType.title"></component>
         </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/cars.jpg">
-        <div class="card-content">
-            Card Content
-        </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/health.jpg">
-        <div class="card-content">
-            Card Content
-        </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/shopping.jpg">
-        <div class="card-content">
-            Card Content
-        </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/travel.jpg">
-        <div class="card-content">
-            Card Content
-        </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-
-<div class="card-item">
-    <div class="card bg-white">
-        <img src="statics/education.jpg">
-        <div class="card-content">
-            Card Content 2
-        </div>
-        <div class="card-actions card-no-top-padding">
-            <div class="auto"></div>
-            <button class="primary small"><i class="on-left">directions</i> Selecionar</button>
-        </div>
-    </div>
-</div>
-
-
-
-
       </div>
     </div>
   </div>
 </template>
+
 <script type="text/javascript">
   /* eslint-disable */
-  import { QCard } from 'quasar'
+  import cardSpendingTypeOne from './cardSpendingTypeOne.vue'
+  import cardSpendingTypeTwo from './cardSpendingTypeTwo.vue'
   export default {
     name: 'SpendingRegister',
     data () {
       return {
-        cardType: 'Vertical One',
-        
+        fullWidth: document.documentElement.clientHeight,
+        spendingTypeData: [
+          {
+            title: 'Alementação',
+            img: 'statics/spending/food.jpg'
+          },
+          {
+            title: 'Compras',
+            img: 'statics/spending/shopping.jpg'
+          },
+          {
+            title: 'Viagens',
+            img: 'statics/spending/travel.jpg'
+          },
+          {
+            title: 'Escola/Faculdade',
+            img: 'statics/spending/education.jpg'
+          },
+          {
+            title: 'Transporte',
+            img: 'statics/spending/cars.jpg'
+          },
+          {
+            title: 'Saúde',
+            img: 'statics/spending/health.jpg'
+          }
+        ]
       }
     },
     components: {
-      QCard
+      cardSpendingTypeOne,
+      cardSpendingTypeTwo
     },
     computed: {
-      
+      componentCardType () {
+        // return this.fullWidth > 600 ? 'cardSpendingTypeOne' : 'cardSpendingTypeTwo';
+        return 'cardSpendingTypeOne';
+      }
     },
     methods: {
-      
+      handleResize (event) {
+        this.fullWidth = document.documentElement.clientWidth
+      }
+    },
+    ready: function () {
+        // window.addEventListener('resize', this.handleResize)
+    },
+    beforeDestroy: function () {
+        // window.removeEventListener('resize', this.handleResize)
     }
   }
 </script>
 <style scoped>
 .card-item{
   float: left;
-}
-
-.card {
-    max-width: 260px;
-  }
-
-.card > img {
-    height: 130px;
 }
 
 
@@ -153,16 +103,5 @@
   .layout-padding.media-control{
     width: 100%;
   }
-
-  .card > img {
-    height: auto !important;
-  }
-
-  .card {
-    max-width: 100%;
-  }
 }
-
-
-
 </style>
