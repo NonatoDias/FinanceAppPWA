7f05cfc26c8a8115a73c9f7ba7b3728a69aeba22
Paulo Jarbas <jarbas@localhost.localdomain>
2018-09-18 21:43:42 -0300

    add reports page

---
+++ src/components/views/reports/reports.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="layout-padding ">
+      <div class="card">
+        <div class="card-title bg-teal text-white">
+          Relatórios
+        </div>
+        <div class="card-content bg-white">
+          <div class="flex">
+            <div class="width-2of3 gt-bg-width-1of3 sm-auto">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    name: 'Reports',
+    data () {
+      return {
+        '3': '5'
+      }
+    },
+    computed: {
+    },
+    components: {
+    },
+    methods: {
+    }
+  }
+</script>
+<style>
+  #map {
+    height: 180px;
+  }
+</style>
