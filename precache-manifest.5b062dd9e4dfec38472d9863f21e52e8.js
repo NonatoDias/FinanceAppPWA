372e425f3d68eebecc27e98ce39be71d01b83c82
Nonato Home <nonato.developer@gmail.com>
2018-10-31 11:14:39 -0300

    adicao offline

--- precache-manifest.5b062dd9e4dfec38472d9863f21e52e8.js
+++ precache-manifest.5b062dd9e4dfec38472d9863f21e52e8.js
@@ -20,8 +20,8 @@ self.__precacheManifest = [
     "url": "js/2d20f70d.759aa09d.js"
   },
   {
-    "revision": "38dc84bfad80f0bc4cb976619dfee3a0",
-    "url": "statics/pwa/finanpe-512.png"
+    "revision": "7b5b351db46b451fcdc586ed752909c3",
+    "url": "statics/pwa/finanpe-192.png"
   },
   {
     "revision": "9a955d12478aca7c6009",
@@ -32,48 +32,48 @@ self.__precacheManifest = [
     "url": "js/4b47640d.cb798a0e.js"
   },
   {
-    "revision": "90564047a7c264831e33613ef0f81565",
-    "url": "statics/user/unknown.jpg"
+    "revision": "25f8fd0caa75a765da4c540351ab0a80",
+    "url": "statics/icons/finanpe-logo.png"
   },
   {
     "revision": "d3043342914116838eb6",
     "url": "js/5d3beef9.854ada60.js"
   },
   {
-    "revision": "e359e3e64146ee68db93c95d224eee2b",
-    "url": "statics/pwa/finanpe-384.png"
+    "revision": "c91486b5bf3819057542d1d5e507a1da",
+    "url": "statics/icons/finanpe-svg.svg"
   },
   {
     "revision": "2d79a35711ed40a21966",
     "url": "js/69c6e5ac.a30c6cb3.js"
   },
   {
-    "revision": "7c4f529783fe3e625fc5da90279e8dae",
-    "url": "statics/pwa/finanpe-256.png"
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "statics/icons/icon-384x384.png"
   },
   {
     "revision": "4a8b03582bcb2df4ca5e",
     "url": "js/74e18795.b5a0d95b.js"
   },
   {
-    "revision": "5f555f833da228d34538e3556c519c72",
-    "url": "statics/pwa/finanpe-128.png"
+    "revision": "90564047a7c264831e33613ef0f81565",
+    "url": "statics/user/unknown.jpg"
   },
   {
     "revision": "7d0a1f4e88055d94a99b",
     "url": "js/app.a4535baf.js"
   },
   {
-    "revision": "7b5b351db46b451fcdc586ed752909c3",
-    "url": "statics/pwa/finanpe-192.png"
+    "revision": "7c4f529783fe3e625fc5da90279e8dae",
+    "url": "statics/pwa/finanpe-256.png"
   },
   {
     "revision": "2b2576d963e9388a278d",
     "url": "js/bcfbe62c.0642e450.js"
   },
   {
-    "revision": "a13b2bd5261e2df374128df1aab93001",
-    "url": "statics/icons/icon-512x512.png"
+    "revision": "38dc84bfad80f0bc4cb976619dfee3a0",
+    "url": "statics/pwa/finanpe-512.png"
   },
   {
     "revision": "902d2f13410e69e30171",
@@ -84,44 +84,44 @@ self.__precacheManifest = [
     "url": "js/vendor.80c5b2b5.js"
   },
   {
-    "revision": "ed12d10fd0407eea42ccaf7af7991146",
-    "url": "statics/icons/ms-icon-144x144.png"
-  },
-  {
-    "revision": "fe4b6b81b55366305919bef83953aa3f",
-    "url": "statics/icons/logo.png"
-  },
-  {
-    "revision": "20c6420ccf16a07d9d56538e30a6b524",
-    "url": "statics/icons/icon-256x256.png"
-  },
-  {
-    "revision": "3020c8ac2c2872dec7741e5948520093",
-    "url": "statics/icons/icon-128x128.png"
-  },
-  {
-    "revision": "66d7719a8ca03c1a591444c013802817",
-    "url": "statics/icons/icon-384x384.png"
-  },
-  {
-    "revision": "319454e307e567022b3e",
-    "url": "js/125e7c68.8f2bc1fd.js"
+    "revision": "e359e3e64146ee68db93c95d224eee2b",
+    "url": "statics/pwa/finanpe-384.png"
   },
   {
     "revision": "8c4120b4e88276badbc808e518572fad",
     "url": "statics/icons/icon-192x192.png"
   },
   {
-    "revision": "25f8fd0caa75a765da4c540351ab0a80",
-    "url": "statics/icons/finanpe-logo.png"
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "statics/icons/ms-icon-144x144.png"
   },
   {
-    "revision": "2f9d7587924933c1912c342b190b53ca",
-    "url": "statics/icons/favicon-32x32.png"
+    "revision": "5f555f833da228d34538e3556c519c72",
+    "url": "statics/pwa/finanpe-128.png"
   },
   {
-    "revision": "efb00e57304380d857603760600da5d5",
-    "url": "statics/icons/favicon-16x16.png"
+    "revision": "fe4b6b81b55366305919bef83953aa3f",
+    "url": "statics/icons/logo.png"
+  },
+  {
+    "revision": "319454e307e567022b3e",
+    "url": "js/125e7c68.8f2bc1fd.js"
+  },
+  {
+    "revision": "a13b2bd5261e2df374128df1aab93001",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "20c6420ccf16a07d9d56538e30a6b524",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
+    "url": "statics/icons/apple-icon-152x152.png"
   },
   {
     "revision": "d4b6a229d7ae188fd8510dfc7a797da2",
@@ -132,12 +132,12 @@ self.__precacheManifest = [
     "url": "statics/bg/bg-mobile.jpg"
   },
   {
-    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
-    "url": "statics/icons/apple-icon-152x152.png"
+    "revision": "2f9d7587924933c1912c342b190b53ca",
+    "url": "statics/icons/favicon-32x32.png"
   },
   {
-    "revision": "c91486b5bf3819057542d1d5e507a1da",
-    "url": "statics/icons/finanpe-svg.svg"
+    "revision": "efb00e57304380d857603760600da5d5",
+    "url": "statics/icons/favicon-16x16.png"
   },
   {
     "revision": "902d2f13410e69e30171",
@@ -164,16 +164,12 @@ self.__precacheManifest = [
     "url": "css/app.353d1603.css"
   },
   {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
     "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
-  },
-  {
-    "revision": "4a8b03582bcb2df4ca5e",
-    "url": "css/74e18795.cea9f9be.css"
-  },
-  {
-    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
-    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
   },
   {
     "revision": "cf6613d1adf490972c557a8e318e0868",
@@ -184,12 +180,16 @@ self.__precacheManifest = [
     "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
   },
   {
-    "revision": "a1471d1d6431c893582a5f6a250db3f9",
-    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
   },
   {
     "revision": "2b2576d963e9388a278d",
     "url": "css/bcfbe62c.83008463.css"
+  },
+  {
+    "revision": "4a8b03582bcb2df4ca5e",
+    "url": "css/74e18795.cea9f9be.css"
   },
   {
     "revision": "2d79a35711ed40a21966",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.1db56a07b473840d08ee75d914bba585.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("precache-manifest.5b062dd9e4dfec38472d9863f21e52e8.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 /**
  * Service worker
@@ -35,7 +35,12 @@ let allExpenses_ = {
     expenses: [{id: 37, value: 20.99, description: 'Teste offline', date: '30/10/2018'}]
 };
 
-const handlerWS = ({url, event, params}) => {
+let lastAddExpense = {};
+
+/**
+ * Get routes
+ */
+workbox.routing.registerRoute(matchWS, ({url, event, params}) => {
     let searchParams = url.searchParams;
     if(searchParams.get('req') === 'expense' && searchParams.get('action') === 'getall'){
         return fetch(event.request)
@@ -53,6 +58,30 @@ const handlerWS = ({url, event, params}) => {
     .then((response) => {
         return response;
     })
-};
+});
 
-workbox.routing.registerRoute(matchWS, handlerWS);
+/**
+ * Post routes
+ */
+workbox.routing.registerRoute(matchWS, ({url, event, params}) => {
+    let searchParams = url.searchParams;
+    if(searchParams.get('req') === 'expense' && searchParams.get('action') === 'addexpense'){
+        event.request.clone().json().then((json_) => {
+            lastAddExpense = json_.data;
+        });
+        return fetch(event.request)
+        .then((response) => {
+            return response;
+
+        }).catch(()=>{
+            lastAddExpense.id = null;
+            allExpenses_.expenses.unshift(lastAddExpense);
+            return createResponseOK(allExpenses_);
+        })
+    }
+    return fetch(event.request)
+    .then((response) => {
+        return response;
+    })
+}, 'POST');
+
