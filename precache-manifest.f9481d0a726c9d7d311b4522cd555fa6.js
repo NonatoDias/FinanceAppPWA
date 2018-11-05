aa8fadc0014f0ca47800117af580ab421e4017c1
Nonato Home <nonato.developer@gmail.com>
2018-11-05 13:56:57 -0300

    deploy 05.11.2018

---
+++ precache-manifest.f9481d0a726c9d7d311b4522cd555fa6.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "fe37446b903ba9218fc2bcbcf936fcf0",
+    "url": "index.html"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/quasar-logo.png"
+  },
+  {
+    "revision": "4e1a9b142b401a7e90bc",
+    "url": "js/2259aa71.a7fe6f53.js"
+  },
+  {
+    "revision": "e359e3e64146ee68db93c95d224eee2b",
+    "url": "statics/pwa/finanpe-384.png"
+  },
+  {
+    "revision": "bd7f5f6c28d0d2006586",
+    "url": "js/2f57bb2d.e537fff4.js"
+  },
+  {
+    "revision": "90564047a7c264831e33613ef0f81565",
+    "url": "statics/user/unknown.jpg"
+  },
+  {
+    "revision": "9a955d12478aca7c6009",
+    "url": "js/3bb05d94.2d61f4d0.js"
+  },
+  {
+    "revision": "4eab671588af6e5de403",
+    "url": "js/4b47640d.cb798a0e.js"
+  },
+  {
+    "revision": "38dc84bfad80f0bc4cb976619dfee3a0",
+    "url": "statics/pwa/finanpe-512.png"
+  },
+  {
+    "revision": "2d79a35711ed40a21966",
+    "url": "js/69c6e5ac.a30c6cb3.js"
+  },
+  {
+    "revision": "7c4f529783fe3e625fc5da90279e8dae",
+    "url": "statics/pwa/finanpe-256.png"
+  },
+  {
+    "revision": "ddb432e61160998cbd68",
+    "url": "js/7f3ad8c6.2ae02f8e.js"
+  },
+  {
+    "revision": "7b5b351db46b451fcdc586ed752909c3",
+    "url": "statics/pwa/finanpe-192.png"
+  },
+  {
+    "revision": "6d5cd76ff0956e279bcc",
+    "url": "js/app.d505229b.js"
+  },
+  {
+    "revision": "5f555f833da228d34538e3556c519c72",
+    "url": "statics/pwa/finanpe-128.png"
+  },
+  {
+    "revision": "1b99500154c5e779961b",
+    "url": "js/b62883b0.07864e60.js"
+  },
+  {
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "9b52a86099d62796fbcb",
+    "url": "js/bf5ad53c.553c78e3.js"
+  },
+  {
+    "revision": "fe4b6b81b55366305919bef83953aa3f",
+    "url": "statics/icons/logo.png"
+  },
+  {
+    "revision": "68b274ac7e45e86c8ec3",
+    "url": "js/c7be0444.3e75a725.js"
+  },
+  {
+    "revision": "dafc2cd5734a2c119208",
+    "url": "js/vendor.80c5b2b5.js"
+  },
+  {
+    "revision": "a13b2bd5261e2df374128df1aab93001",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "20c6420ccf16a07d9d56538e30a6b524",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "c91486b5bf3819057542d1d5e507a1da",
+    "url": "statics/icons/finanpe-svg.svg"
+  },
+  {
+    "revision": "319454e307e567022b3e",
+    "url": "js/125e7c68.8f2bc1fd.js"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "25f8fd0caa75a765da4c540351ab0a80",
+    "url": "statics/icons/finanpe-logo.png"
+  },
+  {
+    "revision": "2f9d7587924933c1912c342b190b53ca",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "efb00e57304380d857603760600da5d5",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "d4b6a229d7ae188fd8510dfc7a797da2",
+    "url": "statics/bg/bg-desktop.jpg"
+  },
+  {
+    "revision": "d5654d8dc38f0fb56325129ccf9461da",
+    "url": "statics/bg/bg-mobile.jpg"
+  },
+  {
+    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "68b274ac7e45e86c8ec3",
+    "url": "css/c7be0444.9ddfc76c.css"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "d5654d8dc38f0fb56325129ccf9461da",
+    "url": "img/bg-mobile.d5654d8d.jpg"
+  },
+  {
+    "revision": "d4b6a229d7ae188fd8510dfc7a797da2",
+    "url": "img/bg-desktop.d4b6a229.jpg"
+  },
+  {
+    "revision": "6d5cd76ff0956e279bcc",
+    "url": "css/app.353d1603.css"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "9b52a86099d62796fbcb",
+    "url": "css/bf5ad53c.17023bb5.css"
+  },
+  {
+    "revision": "1b99500154c5e779961b",
+    "url": "css/b62883b0.6c99ce9e.css"
+  },
+  {
+    "revision": "ddb432e61160998cbd68",
+    "url": "css/7f3ad8c6.6d920793.css"
+  },
+  {
+    "revision": "2d79a35711ed40a21966",
+    "url": "css/69c6e5ac.71f1418b.css"
+  },
+  {
+    "revision": "9a955d12478aca7c6009",
+    "url": "css/3bb05d94.4825c87f.css"
+  },
+  {
+    "revision": "bd7f5f6c28d0d2006586",
+    "url": "css/2f57bb2d.edc9769c.css"
+  },
+  {
+    "revision": "4e1a9b142b401a7e90bc",
+    "url": "css/2259aa71.e6ddc657.css"
+  },
+  {
+    "revision": "319454e307e567022b3e",
+    "url": "css/125e7c68.32b7134b.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.5b062dd9e4dfec38472d9863f21e52e8.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("precache-manifest.f9481d0a726c9d7d311b4522cd555fa6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 /**
  * Service worker
