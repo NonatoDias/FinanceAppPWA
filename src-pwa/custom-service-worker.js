6d1f9337b3ec591dc05fb779c424b5430485c865
Nonato Home <nonato.developer@gmail.com>
2018-11-07 00:57:35 -0300

    backgroundSync

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -4,7 +4,8 @@
  */
 
 let prefix__ = "finanpe";
-let urlWS_ = 'psi-webdias.azurewebsites.net';
+let hostWS_ = 'psi-webdias.azurewebsites.net';
+let urlWS_ = 'https://'+hostWS_+'/';
 workbox.core.setCacheNameDetails({prefix: prefix__});
 
 
@@ -81,7 +82,7 @@ function getActionCallback(searchParams, method){
  * @param {*} param0 
  */
 const matchWSGET = ({url, event}) => {
-    if(url.host.includes(urlWS_)){
+    if(url.host.includes(hostWS_)){
         let searchParams = url.searchParams;
         return getActionCallback(searchParams, 'GET') ? true : false;
     }
@@ -96,16 +97,6 @@ workbox.routing.registerRoute(matchWSGET, workbox.strategies.staleWhileRevalidat
             maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
         }),
         {
-            //cachedResponseWillBeUsed: async ({cacheName, request, matchOptions, cachedResponse, event}) => {
-            /*cacheWillUpdate: async ({request, response, event})  => {
-                let url = new URL(request.url);
-                let searchParams = url.searchParams;
-                let act_ = getActionCallback(searchParams, 'GET');
-                if(act_){
-                    return await act_(url, request, response);
-                }
-                return response;
-            },*/
             cacheWillUpdate: async ({request, response, event})  => {
                 return response.clone().json().then((json_) => {
                     json_.status_cached = true;
@@ -126,128 +117,52 @@ workbox.routing.registerRoute(matchWSGET, workbox.strategies.staleWhileRevalidat
  * @param {*} param0 
  */
 const matchWSPOST = ({url, event}) => {
-    if(url.host.includes(urlWS_)){
+    if(url.host.includes(hostWS_)){
         let searchParams = url.searchParams;
         return getActionCallback(searchParams, 'POST') ? true : false;
     }
     return false;
 };
 
+const queue = new workbox.backgroundSync.Queue(prefix__+'-bck_sync', {
+    maxRetentionTime: 30 * 24 * 60 // Retry for max n minutes
+});
+
 workbox.routing.registerRoute(matchWSPOST, workbox.strategies.networkOnly({
     plugins: [
-        new workbox.backgroundSync.Plugin(prefix__+'-bck_sync', {
-            maxRetentionTime: 30 * 24 * 60 // Retry for max of 24 Hours
-        })
+        {
+            fetchDidFail: async ({originalRequest, request, error, event}) => {
+                queue.addRequest(request);
+            }
+        }
     ],
 }), 'POST');
 
 
+/************************************************************* */
+let loopInterval_ = null;
 
+function loop_(){
+    clearTimeout(loopInterval_)
 
-
-
-
-
-
-
-
-
-
-/******************************************************** */
-
-/**
- * GET routes
- */
-
-
-/*
-const matchWS = ({url, event}) => {
-    return (url.host.includes('psi-webdias.azurewebsites.net'));
-};
-
-workbox.routing.registerRoute(matchWS, ({url, event, params}) => {
-    let searchParams = url.searchParams;
-    let routeFromReq_ = routesFromReq[searchParams.get('req')];
-
-    //Has route ?
-    if(routeFromReq_ && routeFromReq_.actions && routeFromReq_.actions.GET){
-        let actions_ = routeFromReq_.actions.GET;
-        const a_ = searchParams.get('action');
-        let act_ = actions_[a_];
-        let store_ = routeFromReq_.store[a_]
-
-        //Has action ?
-        if(act_ instanceof Function){
-            return fetch(event.request).then((response) => {
-                return response.json().then((json_)=>{ 
-                    act_(json_)
-                    return createResponseOK(json_);
+    loopInterval_ = setTimeout(()=>{
+        console.log('\nSincronizando com ws '+ new Date() +' ... ');
+        fetch(urlWS_ + '?req=api&action=sync').then(function(resp_){
+            return resp_.json().then(function(json) {
+                queue.replayRequests().then(()=>{
+                    console.log('   ok replayed')
+                    loop_();
+                }).catch(()=>{
+                    console.error('   error replayed')
+                    loop_();
                 })
-
-            }).catch(()=>{
-                return createResponseOK(store_);
             })
-        }
-    }
-    //else
-    return fetch(event.request).then((response) => {
-        return response;
-    })
-});
-
-*/
-
-/**
- * Post routes
- *//*
-workbox.routing.registerRoute(matchWS, ({url, event, params}) => {
-    let searchParams = url.searchParams;
-    let routeFromReq_ = routesFromReq[searchParams.get('req')];
-
-    //Has route ?
-    if(routeFromReq_ && routeFromReq_.actions && routeFromReq_.actions.POST){
-        let actions_ = routeFromReq_.actions.POST;
-        let act_ = actions_[searchParams.get('action')];
-
-        //Has action ?
-        if(act_ instanceof Function){
-            return fetch(event.request).then((response) => {
-                return response.json().then((json_)=>{ 
-                    act_(json_)
-                    return createResponseOK(json_);
-                })
-
-            }).catch(()=>{
-                return createResponseOK();
-            })
-        }
-    }
-    //else
-    return fetch(event.request).then((response) => {
-        return response;
-    })
-}, 'POST');
-*/
-/*
-workbox.routing.registerRoute(matchWS, ({url, event, params}) => {
-    let searchParams = url.searchParams;
-    if(searchParams.get('req') === 'expense' && searchParams.get('action') === 'addexpense'){
-        event.request.clone().json().then((json_) => {
-            lastAddExpense = json_.data;
-        });
-        return fetch(event.request)
-        .then((response) => {
-            return response;
-
         }).catch(()=>{
-            lastAddExpense.id = null;
-            allExpenses_.expenses.unshift(lastAddExpense);
-            return createResponseOK(allExpenses_);
+            console.error('NOT sync, maybe offline')
+            loop_();
         })
-    }
-    return fetch(event.request)
-    .then((response) => {
-        return response;
-    })
-}, 'POST');
-*/
+
+    }, 60 * 1000)
+}
+
+loop_();
